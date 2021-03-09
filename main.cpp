bd6a1a1a846fad583c93bbe811fd3460b57d4b3a
Yang Li <yang.li@alertinnovation.com>
2021-03-08 22:20:07 -0500

    Added vehicle class with overloaded function call operator.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
+#include <chrono>
 #include <iostream>
 #include <thread>
-#include <chrono>
 
 void threadFunction()
 {
-    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // simulate work
-    std::cout << "Finished work in thread\n";
+    std::cout << "The other thread Thread id = " << std::this_thread::get_id() << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));  // simulate work
+    std::cout << "Finished work 1 in thread\n";
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "Finished work 2 in thread\n";
 }
+
+class Vehicle
+{
+public:
+    Vehicle(int id) : _id(id) {}
+    void operator()()
+    {
+        std::cout << "Vehicle #" << _id << " has been created" << std::endl;
+    }
+
+private:
+    int _id;
+};
 
 int main()
 {
@@ -15,14 +33,19 @@ int main()
     std::cout << "This machine supports concurrency with " << nCores << " cores available" << std::endl;
 
     // create thread
-    std::thread t(threadFunction);
+    std::thread t {threadFunction};
+    std::thread tV {Vehicle(1)};
 
     // do something in main()
-    std::this_thread::sleep_for(std::chrono::milliseconds(50)); // simulate work
-    std::cout << "Finished work in main\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));  // simulate work
+    std::cout << "Finished work 1 in main\n";
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "Finished work 2 in main\n";
 
     // wait for thread to finish
     t.join();
+    tV.join();
 
     return 0;
 }
